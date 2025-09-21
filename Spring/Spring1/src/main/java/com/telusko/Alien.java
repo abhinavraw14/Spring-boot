a2ba0897dbf5a092700479e43c0099ea6db3f8bf
abhinavraw14 <abhinavrawat1401@gmail.com>
2025-09-21 13:17:46 +0530

    same object different references

--- Spring/Spring1/src/main/java/com/telusko/Alien.java
+++ Spring/Spring1/src/main/java/com/telusko/Alien.java
@@ -1,6 +1,7 @@
 package com.telusko;
 
 public class Alien {
+    int age;
     public Alien(){
         System.out.println("Object Created");
     }
--- Spring/Spring1/src/main/java/com/telusko/App.java
+++ Spring/Spring1/src/main/java/com/telusko/App.java
@@ -9,9 +9,15 @@ public class App
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml"); //This line basically creates a container for you.
         //Xml based configuration
         Alien obj1 = (Alien) context.getBean("alien1"); //getBean(XML id);
-        obj1.code();
+        obj1.age = 21;
+
+        System.out.println(obj1.age);
+        //obj1.code();
+
         Alien obj2 = (Alien) context.getBean("alien1"); //getBean(XML id);
-        obj2.code();
+        System.out.println(obj2.age);
+//      even if we have 2 different referencees but the object is same.
+        //obj2.code();
 
     }
 }
